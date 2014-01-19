3a25224b1499230e182d7b770d9ea3f085e6c7cf
Luciano Facchinelli <facchinelli.luciano@gmail.com>
2014-01-19 15:18:53 -0200

    creating funcs and adding user-agent management

--- wallbase_crawler.py
+++ wallbase_crawler.py
@@ -1,17 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
 import urllib
+import random
 
-r = requests.get("http://wallbase.cc/collection/29053")
+user_agents = [
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+    'Opera/9.25 (Windows NT 5.1; U; en)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.142 Safari/535.19',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:8.0.1) Gecko/20100101 Firefox/8.0.1',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.151 Safari/535.19'
+]
+baseurl = "http://wallbase.cc/collection/29053"
+headers = { 'User-Agent' : random.choice(user_agents) }
+dir = "/Users/usuario/Pictures/wallpapers-mac/wallpaper"
 
-data = r.text
+def GetData(baseurl):
 
-soup = BeautifulSoup(data)
+    r = requests.get(baseurl, headers=headers)
 
-for link in soup.find_all('a', target="_blank"):
-    url = link.get('href')
-    cortado = url.split("/")
-    urlf = "http://wallpapers.wallbase.cc/rozne/wallpaper-" +cortado[4] + ".jpg"
-    urllib.urlretrieve(urlf, "/Users/usuario/Pictures/wallpapers-mac/wallpaper" +cortado[4] + ".jpg")
+    data = r.text
     
-    
+    soup = BeautifulSoup(data)
+
+    return soup
+
+def CrawlData (soup):
+    for link in soup.find_all('a', target="_blank"):
+        url = link.get('href')
+        print url
+    '''    cortado = url.split("/")
+        urlf = "http://wallpapers.wallbase.cc/rozne/wallpaper-" +cortado[4] + ".jpg"
+        urllib.urlretrieve(urlf, dir +cortado[4] + ".jpg")
+    '''
+origen = GetData(baseurl)
+CrawlData(origen)    
